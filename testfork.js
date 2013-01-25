b4586452520e17e0caf0e82e2caa3e72dea0c9ad
zhaojiangang <37294812@qq.com>
2013-01-25 10:29:41 +0800

    Update testfork.js
    删除了不用的注释，给gruntJs增加了setName方法

--- testfork.js
+++ testfork.js
@@ -1,10 +1,6 @@
-//add comment
-//hello world!
-//add comment
-//2013-1-25
-//add commnet
-//2013年1月25日星期五
-
 var gruntJs=function(){
 	this.username='zhaojiangang';
+	this.setName=function(name){
+		this.username=name;
+	}
 }
